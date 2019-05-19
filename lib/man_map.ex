278b5d23513ce2f84594a4b112e852cea9e3382d
Steve DeGele <sdegele@gmail.com>
2019-05-18 22:11:22 -0400

    initial commit

---
+++ lib/man_map.ex
@@ -0,0 +1,18 @@
+defmodule ManMap do
+  @moduledoc """
+  Documentation for ManMap.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ManMap.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/man_map_test.exs
@@ -0,0 +1,8 @@
+defmodule ManMapTest do
+  use ExUnit.Case
+  doctest ManMap
+
+  test "greets the world" do
+    assert ManMap.hello() == :world
+  end
+end
